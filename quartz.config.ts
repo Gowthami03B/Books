d20dfa0f8a68cf891d6660cd40914ba659f7640e
Gowthami B <gowthamibhogireddy@gmail.com>
2025-08-20 08:24:45 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "Gowthami03B.github.io/Books",
+    baseUrl: "gowthami03b.github.io/Books",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
